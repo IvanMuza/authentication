fd0c53edb2cea4b93fbd1451121dc242dbe64ee9
Ivan Muza <ivan.muza12@gmail.com>
2025-09-06 09:10:06 -0500

    feat: create Role class and update schema of database

---
+++ infrastructure/driven-adapters/security/src/main/java/co/com/crediya/security/JwtProviderConfig.java
@@ -0,0 +1,44 @@
+package co.com.crediya.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtProviderConfig {
+    private final String secretKey = "my-secret-key";
+    private final long validityInMs = 3600000;
+
+    public String createToken(String email, String role) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMs);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .claim("role", role)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
